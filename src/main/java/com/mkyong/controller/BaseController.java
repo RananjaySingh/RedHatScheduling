48a63332343aed39021231fcb30e1ead24fa1e0e
RananjaySingh <30312241+RananjaySingh@users.noreply.github.com>
2017-09-14 15:25:28 +0530

    Update BaseController.java

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -2,13 +2,16 @@ package com.mkyong.controller;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BaseController {
 	
 	@Autowired
@@ -38,8 +41,22 @@ public class BaseController {
 		model.addAttribute("message", "Welcome " + name);
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcomeName] counter : {}", counter);
-		return VIEW_INDEX;
+		return VIEW_INDEX;```````````````````
 
 	}*/
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public ResponseEntity test() {
+
+	/*	model.addAttribute("message", "Welcome");
+		model.addAttribute("counter", ++counter);
+		logger.debug("[welcome] counter : {}", counter);*/
+
+		// Spring uses InternalResourceViewResolver and return back index.jsp
+		String test=app.test();
+		System.out.println("Test controller");
+		/*logger.info("HI OPENSHIFT");*/
+		return new ResponseEntity(test, HttpStatus.OK);
+
+	}
 
 }
